04ab93916a04b377c8da32ce8c4d1798e230ffbf
MichelleBKlynn <michellestaton@gmail.com>
2016-09-14 12:32:01 -0400

    Complete answers

--- Dictionaries.playground/Contents.swift
+++ Dictionaries.playground/Contents.swift
@@ -28,3 +28,58 @@ definition = favoriteWords["strategery"]
 
 favoriteWords["strategery"] = nil
 print(favoriteWords)
+
+
+
+var favoritePlaces = [
+    3: "Hanalei",
+    4: "New York City"
+]
+
+// idk why I can't do 1: "Hanalei", 2: "New York City" and have it print out as that.
+
+
+var favoritePlacesStringFormat = [
+    "Number One": "Hanalei",
+    "Number Two": "New York City"
+]
+
+var favoritePlace = favoritePlaces[3]
+var secondFavoritePlace = favoritePlaces[4]
+
+if let favoritePlace = favoritePlace {
+if let secondFavoritePlace = secondFavoritePlace {
+print("Michelle's absolute favorite place in the world is '\(favoritePlace)', followed by '\(secondFavoritePlace)'.")
+}
+}
+
+var leastFavoritePlaces = [3: "cold places", 4: "desearts"]
+
+if let leastFavoritePlace = leastFavoritePlaces[3] {
+    print("Michelle hates '\(leastFavoritePlace)'.")
+}
+
+var favoriteCandyBars = [
+"Twix": "Because it's the best",
+"Milkey Way": "Because it's good, too"
+]
+
+var whyFavoriteCandyBar = favoriteCandyBars["Twix"]
+
+var nonexistantFavoriteCandyBars = favoriteCandyBars["Snickers"]
+
+if let whyFavoriteCandyBar = nonexistantFavoriteCandyBars {
+    print("She likes Twix because \(whyFavoriteCandyBar)")
+} else {
+    print("She hates Snickers")
+}
+
+favoriteCandyBars["Snickers"] = "Snickers"
+
+whyFavoriteCandyBar = favoriteCandyBars["Snickers"]
+
+favoriteCandyBars["Snickers"] = nil
+
+print(favoriteCandyBars)
+
+
